305ebcf674e87e3c278e973171896427dc698f4e
camicasii <camicasii@hotmail.com>
2019-09-03 18:24:29 -0400

    mi pagina

---
+++ precache-manifest.132c26078d95c38fc23ba85e1c1f8b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3ea8b2c1f82bb13f164fd50dfa3598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ff257da0fa35e34afa7",
+    "url": "/static/css/main.13975a8c.chunk.css"
+  },
+  {
+    "revision": "32f12a9997747d552c1e",
+    "url": "/static/js/2.75bbbdcd.chunk.js"
+  },
+  {
+    "revision": "4ff257da0fa35e34afa7",
+    "url": "/static/js/main.359bdac9.chunk.js"
+  },
+  {
+    "revision": "52fbba9152ef21b9a018",
+    "url": "/static/js/runtime~main.551c73ac.js"
+  }
+]);
